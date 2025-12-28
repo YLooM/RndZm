cf5f37e4f94b223bf25aae16dc67b6906e53452e
YLooM <yloomlebg@gmail.com>
2025-12-27 20:23:46 -0500

    Ajoutez des fichiers projet.

---
+++ RndZm_yl1/RndZm_yl1.cpp
@@ -0,0 +1,12 @@
+﻿// RndZm_yl1.cpp : définit le point d'entrée de l'application.
+//
+
+#include "RndZm_yl1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
