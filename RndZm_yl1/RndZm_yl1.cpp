e3320f822eab149eab542809c06db0ed41fe2bc6
YLooM <yloomlebg@gmail.com>
2025-12-28 14:08:46 -0500

    added drawing mehtods and struct rework

--- RndZm_yl1/RndZm_yl1.cpp
+++ RndZm_yl1/RndZm_yl1.cpp
@@ -4,11 +4,19 @@
 
 
 using namespace std;
-void cleanup(SDL_Window* win);
+struct SDLState
+{
+	SDL_Window *window;
+	SDL_Renderer* renderer;
+};
+
+
+
+void cleanup(SDLState& state);
 
 int main(int argc, char *argv[])
 {
-	
+	SDLState state;
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
 		
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", nullptr);
@@ -16,21 +24,33 @@ int main(int argc, char *argv[])
 		
 	}
 	
-	//Creation de la fenetre
+	//Window base settings
 
 	int width = 1280;
 	int height = 720;
-	SDL_Window* win = SDL_CreateWindow("RndZm", width, height, 0);
+	state.window = SDL_CreateWindow("RndZm", width, height, 0);
 	
-	if (!win) {
+	if (!state.window) {
 
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", nullptr);
-		cleanup(win);
+		cleanup(state);
 		return 1;
 
 	}
 
-	//Creation du main game loop
+	//Renderer
+	state.renderer = SDL_CreateRenderer(state.window, nullptr);
+	if (!state.renderer) {
+
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", state.window);
+		cleanup(state);
+		return 1;
+
+	}
+
+
+	//Main game loop
+
 	bool running = true;
 	while (running) 
 	{
@@ -50,14 +70,23 @@ int main(int argc, char *argv[])
 		
 		}
 	
+		//Drawing
+		
+		SDL_SetRenderDrawColor(state.renderer, 0, 255, 0, 255);
+		SDL_RenderClear(state.renderer);
+
+		//flip backbufer to front
+		SDL_RenderPresent(state.renderer);
+
 	}
 
-	cleanup(win);
+	cleanup(state);
 	return 0;
 }
 
-void cleanup(SDL_Window* win) {
-	SDL_DestroyWindow(win);
+void cleanup(SDLState &state) {
+	SDL_DestroyRenderer(state.renderer);
+	SDL_DestroyWindow(state.window);
 	SDL_Quit();
 
 }
